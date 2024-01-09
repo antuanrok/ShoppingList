d527d9e3a7d56f0aada2a4ce6b0bccb474e8af9b
antuanrok <a-gen@yandex.ru>
2024-01-09 14:17:43 +0300

    Lesson11. ViewModel для ShopItemActivity

---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewModel.kt
@@ -0,0 +1,118 @@
+package com.example.shoppinglist.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.data.ShopListRepositoryImpl
+import com.example.shoppinglist.domain.AddShopItemUseCase
+import com.example.shoppinglist.domain.DeleteShopItemUseCase
+import com.example.shoppinglist.domain.EditShopItemUseCase
+import com.example.shoppinglist.domain.GetShopItemUseCase
+import com.example.shoppinglist.domain.GetShopListUseCase
+import com.example.shoppinglist.domain.ShopItem
+import java.util.concurrent.CountDownLatch
+
+class ShopItemViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+
+    private var _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private var _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputCount
+
+    private var _shopItem = MutableLiveData<ShopItem>()
+    val shopItems: LiveData<ShopItem>
+        get() = _shopItem
+
+    private var _canCloseAcivity = MutableLiveData<Unit>()
+    val canCloseAcivity: LiveData<Unit>
+        get() = _canCloseAcivity
+
+
+    // var shopList = MutableLiveData<List<ShopItem>>()
+    // var shopList = getShopListUseCase.getShopList()
+
+    /*fun getShopList() {
+        //val list = getShopListUseCase.getShopList()
+        //shopList.postValue(list)
+        getShopListUseCase.getShopList()
+    }*/
+
+    fun getShopItem(id: Int) {
+        val item = getShopItemUseCase.getShopItem(id)
+        _shopItem.postValue(item)
+    }
+
+    fun addElement(name: String?, count: String?) {
+        val name = parseName(name)
+        val count = parseCount(count)
+        val fieldsOk = validateInput(name, count)
+        if (fieldsOk) {
+            val shopItem = ShopItem(name, count, true)
+            addShopItemUseCase.addShopItem(shopItem)
+            finishWork()
+        }
+    }
+
+    fun editElement(name: String?, count: String?, id: Int) {
+        val name = parseName(name)
+        val count = parseCount(count)
+        val fieldsOk = validateInput(name, count)
+        if (fieldsOk) {
+            _shopItem.value?.let {
+                val item = it.copy(name=name,count = count)
+                editShopItemUseCase.editShopItem(item)
+                finishWork()
+            }
+        }
+
+
+    }
+
+    private fun parseCount(input: String?): Int {
+        return try {
+            input?.trim()?.toInt() ?: 0
+        } catch (e: Exception) {
+            0
+        }
+
+    }
+
+    private fun parseName(input: String?): String {
+        return input?.trim() ?: ""
+    }
+
+    private fun validateInput(name: String, count: Int): Boolean {
+        var res = true
+        if (name.isBlank()) {
+            _errorInputName.postValue(true)
+            res = false
+        }
+        if (count <= 0) {
+            _errorInputCount.postValue(true)
+            res = false
+        }
+        return res
+    }
+
+    fun resetErrorInputName() {
+        _errorInputName.postValue(false)
+    }
+
+    fun resetErrorInputCount() {
+        _errorInputCount.postValue(false)
+    }
+
+    fun finishWork() {
+        _canCloseAcivity.postValue(Unit)
+    }
+
+}
